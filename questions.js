e96e1cb2be86bbac920e914fdacb10f5ad83ac1d
Solange <72896376+macintyresj@users.noreply.github.com>
2025-10-18 19:19:38 -0300

    Refactor questions.js for formatting and syntax
    Formatted the questions array for better readability and fixed image URL syntax.

--- questions.js
+++ questions.js
@@ -1,11 +1,51 @@
+/** @format */
+
 // questions.js
 const questions = [
-  { id:1, question:"¿Quién fue el vocalista de Queen?", options:["Freddie Mercury","David Bowie","Elton John","Mick Jagger"], correct:0, points:100, image:null },
-  { id:2, question:"¿En qué año se lanzó 'Nevermind' de Nirvana?", options:["1989","1991","1993","1995"], correct:1, points:100, image: url("images/album.png" },
-  { id:3, question:"¿A quién pertenece esta camiseta?", options:["Diego Maradona","Lionel Messi","Pele","Cristiano Ronaldo"], correct:1, points:100, image:"https://upload.wikimedia.org/wikipedia/commons/8/84/Argentina_national_football_team_2014-05-01_11.jpg" },
-  { id:4, question:"¿Cuál de estos estilos NO es parte del rock?", options:["Grunge","Ska","Barroco","Punk"], correct:2, points:100, image:null },
-  { id:5, question:"¿Qué banda compuso 'Smells Like Teen Spirit'?", options:["Pearl Jam","Alice in Chains","Nirvana","Soundgarden"], correct:2, points:100, image:null }
+  {
+    id: 1,
+    question: "¿Quién fue el vocalista de Queen?",
+    options: ["Freddie Mercury", "David Bowie", "Elton John", "Mick Jagger"],
+    correct: 0,
+    points: 100,
+    image: null,
+  },
+  {
+    id: 2,
+    question: "¿En qué año se lanzó 'Nevermind' de Nirvana?",
+    options: ["1989", "1991", "1993", "1995"],
+    correct: 1,
+    points: 100,
+    image: "images/album.png",
+  },
+  {
+    id: 3,
+    question: "¿A quién pertenece esta camiseta?",
+    options: ["Diego Maradona", "Lionel Messi", "Pele", "Cristiano Ronaldo"],
+    correct: 1,
+    points: 100,
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/8/84/Argentina_national_football_team_2014-05-01_11.jpg",
+  },
+  {
+    id: 4,
+    question: "¿Cuál de estos estilos NO es parte del rock?",
+    options: ["Grunge", "Ska", "Barroco", "Punk"],
+    correct: 2,
+    points: 100,
+    image: null,
+  },
+  {
+    id: 5,
+    question: "¿Qué banda compuso 'Smells Like Teen Spirit'?",
+    options: ["Pearl Jam", "Alice in Chains", "Nirvana", "Soundgarden"],
+    correct: 2,
+    points: 100,
+    image: null,
+  },
 ];
+
+
 
 
 
